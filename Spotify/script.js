34ace5192569b90eb21a854c5d4b466051c0443d
Prabin Karki <karkiprabin198@gmail.com>
2024-08-28 22:25:23 +0545

    modified

--- Spotify/script.js
+++ Spotify/script.js
@@ -8,7 +8,7 @@ let gif=document.getElementById('gif');
 let songList=Array.from(document.querySelectorAll('.songsList'))
 let end=document.querySelector('#end');
 let songs=[
-    {songName:'Dancing in the star',filePath:'songs/1.mp3',coverPath:'song images/1.webp',timestamp:'01:58'},
+    {songName:'Dancing in the stardust',filePath:'songs/1.mp3',coverPath:'song images/1.webp',timestamp:'01:58'},
     {songName:'Cartooon X Jeja,On & On',filePath:'songs/2.mp3',coverPath:'song images/2.jpg',timestamp:'03:28'},
     {songName:'Bring Me back',filePath:'songs/3.mp3',coverPath:'song images/3.jpg',timestamp:'03:50'},
     {songName:'Royality',filePath:'songs/4.mp3',coverPath:'song images/4.jpg',timestamp:'03:43'},
@@ -27,15 +27,20 @@ masterplay.addEventListener('click',()=>{
     if(audioElement.paused ||audioElement.currentTime<=0){
         audioElement.play();
         masterplay.classList.remove('fa-play');
-        masterplay.classList.add('fa-pause')
+        masterplay.classList.add('fa-pause');
         gif.style.opacity=1;
         end.innerText=songs[songIndex].timestamp;
+       
     }
     else{
         audioElement.pause();
         masterplay.classList.remove('fa-pause');
         masterplay.classList.add('fa-play')
         gif.style.opacity=0;
+        if(songItemPlay.played){
+            songItemPlay.classList.remove('fa-play');
+            songItemPlay.classList.add('fa-pause');
+        }
     }
 
 })
@@ -52,33 +57,55 @@ myProgressBar.addEventListener('click',()=>{
 })
 const makeItemPlay=function(){
     Array.from(document.getElementsByClassName('songItemPlay')).forEach((e)=>{
-        // audioElement.pause();
-        e.classList.remove('fa-pause')
-        e.classList.add('fa-play')
-        
-    })
-}
-Array.from(document.getElementsByClassName('songItemPlay')).forEach( (e)=>{
-    e.addEventListener('click',(element)=>{
-      makeItemPlay();
-      songIndex=parseInt(element.target.id);
-      element.target.classList.remove('fa-play');
-      element.target.classList.add('fa-pause')
-      audioElement.src=`songs/${songIndex+1}.mp3`;
-      audioElement.currentTime=0;
-      audioElement.play();
-      masterplay.classList.remove('fa-play');
-      masterplay.classList.add('fa-pause')
-      gif.style.opacity=1;
-      masterName.innerText=songs[songIndex].songName;
-      end.innerText=songs[songIndex].timestamp;
       
+      
+     
+        e.classList.remove('fa-pause')
+        e.classList.add('fa-play');
+
+        
+    })    
+    
+}
+const songItemPlay=Array.from(document.getElementsByClassName('songItemPlay'))
+songItemPlay.forEach( (e)=>{
+    e.addEventListener('click',(element)=>{
+        
+        songIndex=parseInt(element.target.id);
+        makeItemPlay();
+     if(audioElement.paused||audioElement<=0){
+      
+    
+        element.target.classList.remove('fa-play');
+        element.target.classList.add('fa-pause')
+        audioElement.src=`songs/${songIndex+1}.mp3`;
+        audioElement.currentTime=0;
+        audioElement.play();
+        masterplay.classList.remove('fa-play');
+        masterplay.classList.add('fa-pause')
+     }
+     else if(audioElement.played){
+        audioElement.pause();
+        element.target.classList.remove('fa-pause');
+        element.target.classList.add('fa-play')
+        masterplay.classList.remove('fa-pause');
+        masterplay.classList.add('fa-play')
+       
+     }
+        gif.style.opacity=1;
+        masterName.innerText=songs[songIndex].songName;
+        end.innerText=songs[songIndex].timestamp;
+        audioElement.currentTime=0;
     })
 })
+
 document.getElementById('previous').addEventListener('click',()=>{
     if(songIndex<=0){
-        songIndex=0;
+        songIndex=10;
+        audioElement.src=`songs/${songIndex}.mp3`;
+        audioElement.play();
     }
+  
     else{
         songIndex -=1
         audioElement.src=`songs/${songIndex}.mp3`;
@@ -91,10 +118,13 @@ document.getElementById('previous').addEventListener('click',()=>{
       gif.style.opacity=1;
       
     }
+    
 })
 document.getElementById('next').addEventListener('click',()=>{
     if(songIndex>9){
         songIndex=0;
+        audioElement.scr=`songs/1.mp3`
+        audioElement.play();
     }
     else{
         songIndex +=1
@@ -106,6 +136,7 @@ document.getElementById('next').addEventListener('click',()=>{
       masterplay.classList.add('fa-pause')
       gif.style.opacity=1;
       masterName.innerText=songs[songIndex -1].songName;
+      
       end.innerText=songs[songIndex-1].timestamp;
     
     }
