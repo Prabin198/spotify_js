6df65ec5c9fb5c3a81da32f29a312ef0a7460188
Prabin Karki <karkiprabin198@gmail.com>
2024-08-28 19:36:43 +0545

    push

--- Spotify/script.js
+++ Spotify/script.js
@@ -6,23 +6,22 @@ let masterplay=document.querySelector('#masterplay');
 let myProgressBar=document.getElementById('myProgressBar');
 let gif=document.getElementById('gif');
 let songList=Array.from(document.querySelectorAll('.songsList'))
+let end=document.querySelector('#end');
 let songs=[
-    {songName:'Dancing in the star',filePath:'songs/1.mp3',coverPath:'song images/1.webp'},
-    {songName:'Cartooon X Jeja,On & On',filePath:'songs/2.mp3',coverPath:'song images/2.jpg'},
-    {songName:'Bring Me back',filePath:'songs/3.mp3',coverPath:'song images/3.jpg'},
-    {songName:'Royality',filePath:'songs/4.mp3',coverPath:'song images/4.jpg'},
-    {songName:'Shree Krishna Flute',filePath:'songs/5.mp3',coverPath:'song images/5.jpeg'},
-    {songName:'Tum Preet Ho Tum Geet Ho',filePath:'songs/6.mp3',coverPath:'song images/6.jpeg'},
-    {songName:'FearLess-II',filePath:'songs/7.mp3',coverPath:'song images/7.jpg'},
-    {songName:'Cradles',filePath:'songs/8.mp3',coverPath:'song images/8.png'},
-    {songName:'One Dance,Drake',filePath:'songs/9.mp3',coverPath:'song images/9.jpeg'}
+    {songName:'Dancing in the star',filePath:'songs/1.mp3',coverPath:'song images/1.webp',timestamp:'01:58'},
+    {songName:'Cartooon X Jeja,On & On',filePath:'songs/2.mp3',coverPath:'song images/2.jpg',timestamp:'03:28'},
+    {songName:'Bring Me back',filePath:'songs/3.mp3',coverPath:'song images/3.jpg',timestamp:'03:50'},
+    {songName:'Royality',filePath:'songs/4.mp3',coverPath:'song images/4.jpg',timestamp:'03:43'},
+    {songName:'Shree Krishna Flute',filePath:'songs/5.mp3',coverPath:'song images/5.jpeg',timestamp:'02:25'},
+    {songName:'Tum Preet Ho Tum Geet Ho',filePath:'songs/6.mp3',coverPath:'song images/6.jpeg',timestamp:'04:08'},
+    {songName:'FearLess-II',filePath:'songs/7.mp3',coverPath:'song images/7.jpg',timestamp:'03:14'},
+    {songName:'Cradles',filePath:'songs/8.mp3',coverPath:'song images/8.png',timestamp:'03:29'},
+    {songName:'One Dance,Drake',filePath:'songs/9.mp3',coverPath:'song images/9.jpeg',timestamp:'02:54'}
 ]
  songList.forEach((e,i)=>{
    e.getElementsByTagName('img')[0].src=songs[i].coverPath;
 //    console.log(songName.innerHtml)
-   e.getElementsByClassName('songName')[0].innerText=songs[i].songName
-   
-
+   e.getElementsByClassName('songName')[0].innerText=songs[i].songName;
  })
 masterplay.addEventListener('click',()=>{
     if(audioElement.paused ||audioElement.currentTime<=0){
@@ -44,7 +43,7 @@ audioElement.addEventListener('timeupdate',()=>
 {
 // console.log('timeupdate')
 let progress=parseInt((audioElement.currentTime/audioElement.duration)*100)
-myProgressBar.value=progress
+myProgressBar.value=progress;
 })
 myProgressBar.addEventListener('click',()=>{
     audioElement.currentTime=myProgressBar.value*audioElement.duration/100;
@@ -60,7 +59,7 @@ const makeItemPlay=function(){
 }
 Array.from(document.getElementsByClassName('songItemPlay')).forEach( (e)=>{
     e.addEventListener('click',(element)=>{
-        makeItemPlay();
+      makeItemPlay();
       songIndex=parseInt(element.target.id);
       element.target.classList.remove('fa-play');
       element.target.classList.add('fa-pause')
@@ -71,7 +70,8 @@ Array.from(document.getElementsByClassName('songItemPlay')).forEach( (e)=>{
       masterplay.classList.add('fa-pause')
       gif.style.opacity=1;
       masterName.innerText=songs[songIndex].songName;
-
+      end.innerText=songs[songIndex].timestamp;
+      
     })
 })
 document.getElementById('previous').addEventListener('click',()=>{
@@ -83,10 +83,12 @@ document.getElementById('previous').addEventListener('click',()=>{
         audioElement.src=`songs/${songIndex}.mp3`;
       audioElement.currentTime=0;
       masterName.innerText=songs[songIndex-1].songName;
+      end.innerText=songs[songIndex-1].timestamp;
       audioElement.play();
       masterplay.classList.remove('fa-play');
       masterplay.classList.add('fa-pause')
       gif.style.opacity=1;
+      
     }
 })
 document.getElementById('next').addEventListener('click',()=>{
@@ -103,6 +105,7 @@ document.getElementById('next').addEventListener('click',()=>{
       masterplay.classList.add('fa-pause')
       gif.style.opacity=1;
       masterName.innerText=songs[songIndex -1].songName;
+      end.innerText=songs[songIndex-1].timestamp;
     
     }
 })
